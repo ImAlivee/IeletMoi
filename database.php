71f8970b0cbcea10d03ee74046748f4230b73c34
ImAlivee <144893180+ImAlivee@users.noreply.github.com>
2023-12-14 15:24:32 +0100

    create database.php

---
+++ database.php
@@ -0,0 +1,25 @@
+<?php
+$temoignages = [
+    'titre' => '',
+    'numero' => '',
+    'contenu' =>'' ,
+    'auteur' =>'' ,
+    'url' => '',
+];
+$membres = [
+    'nom' => '',
+    'prenom' => '',
+    'photo' => '',
+    'url' => '',
+];
+$menu =[
+    'Acceuil' => 'accueil.php',
+    'Formulaire' => 'formulaire.php',
+    'Témoignage' => 'temoignages.php',
+    'Présentation' => 'presentation.php',
+    'Contact' => 'contact.php',
+];
+$footer =[ 
+    'mentions' => 'mentionslegales',
+    'copyright' => ' ',
+]
