826936aa83ac9f631a6c16db0e754073776e6c53
ImAlivee <aurore.regnier@etu.u-pec.fr>
2023-12-21 14:49:15 +0100

    html accueil

--- app/View/accueil.view.php
+++ app/View/accueil.view.php
@@ -1,7 +1,8 @@
 <section class="description">
             <div class="texte">
             <h1>IEL & MOI</h1>
-            <p>En quoi consiste notre association :</p>
+            <h3>Vers les autres, pas à pas</h3>
+            <p>En quoi consiste notre association :Notre collectif 'iel et moi' a constaté que le mauvais emploi des pronoms mpacte quotidiennement une partie de la communauté LGBTQIA+. Nous avons pour but de sensibiliser à ce sujet avec des témoignages retranscrits sous forme de BD podcasts et à l'écrit. Notre but : transmettre des connaisance sur la culture LGBTQIA+ et les notions liée aux identitées de genre et à l'usage des nouveaux pronoms aux jeunes alliées de 14 à 25 ans.</p>
             </div>
         </section>
         <section class="preview">
