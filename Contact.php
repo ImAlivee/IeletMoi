4083964031169851a441e409fc20f71c5a509931
RedEmperor7 <73673106+RedEmperor7@users.noreply.github.com>
2024-01-03 17:38:02 +0100

    database_team_update_1
    Les noms et prénoms

--- Contact.php
+++ Contact.php
@@ -1,9 +1,7 @@
 <?php
 $css = 'contact.css';
-
 ob_start();
 include 'app/view/contact.view.php';
 $content = ob_get_clean();
 
 include 'app/view/common/layout.php';
-?>
--- database.php
+++ database.php
@@ -2,12 +2,12 @@
 $temoignages_bd = [
     'titre' => '',
     'numero' => '',
-    'contenu' =>'' ,
+    'contenu' => '',
     'url' => '',
     'auteur' => [
-        'nom'=> '',
-        'prenom'=>'',
-        'age'=>'',
+        'nom' => '',
+        'prenom' => '',
+        'age' => '',
     ],
 ];
 $temoignages_oral = [
@@ -16,7 +16,7 @@ $temoignages_oral = [
     'auteur' => [
         'nom' => 'Lemoine',
         'prenom' => 'Emma',
-        'age' => '' , 
+        'age' => '',
     ]
 ];
 /* ajouter témoignage de auteur: Aurore et témoignant Léo; auteur Alban témoignant Ambre ; auteur Alicia témoignant Valentin ; auteur Emilie témoignant Emma */
@@ -24,27 +24,71 @@ $temoignages_oral = [
 
 $temoignages_ecrit = [
     'titre' => '',
-    'temoignages' =>'',
+    'temoignages' => '',
     'auteur' => [
-        'nom'=> '',
-        'prenom'=>'',
-        'age'=>'',
+        'nom' => '',
+        'prenom' => '',
+        'age' => '',
     ],
 ];
 $membres = [
-    'nom' => '',
-    'prenom' => '',
-    'photo' => '',
-    'url' => '',
+    1 => [
+        'nom' => 'Regnier',
+        'prenom' => 'Aurore',
+        'photo' => '',
+        'url' => '',
+    ],
+    2 => [
+        'nom' => 'Faitout-Fourmigué',
+        'prenom' => 'Alban',
+        'photo' => '',
+        'url' => '',
+    ],
+    3 => [
+        'nom' => 'Leclaire',
+        'prenom' => 'Raphaël',
+        'photo' => '',
+        'url' => '',
+    ],
+    4 => [
+        'nom' => 'Martin',
+        'prenom' => 'Joshuoy',
+        'photo' => '',
+        'url' => '',
+    ],
+    5 => [
+        'nom' => 'Becker',
+        'prenom' => 'Émilie',
+        'photo' => '',
+        'url' => '',
+    ],
+    6 => [
+        'nom' => 'Lemoine',
+        'prenom' => 'Emma',
+        'photo' => '',
+        'url' => '',
+    ],
+    7 => [
+        'nom' => 'Gossiome',
+        'prenom' => 'Alicia',
+        'photo' => '',
+        'url' => '',
+    ],
+    8 => [
+        'nom' => 'Moudio',
+        'prenom' => 'Nathân',
+        'photo' => '',
+        'url' => '',
+    ],
 ];
-$menu =[
+$menu = [
     'Accueil' => 'accueil.php',
     'Formulaire' => 'formulaire.php',
     'Temoignage' => 'temoignages.php',
     'Présentation' => 'presentation.php',
     'Contact' => 'contact.php',
 ];
-$footer =[ 
+$footer = [
     'mentions' => 'mentions_legales.php',
     'copyright' => ' ',
 ];
