cf02d13d644d9550d8eeb8ee0a09778b6c2204ac
Kyokanmi <superemmabros@gmail.com>
2024-01-13 14:39:10 +0100

    ajout page perso Emma

---
+++ app/View/emma.view.php
@@ -0,0 +1,68 @@
+            <div class ="haut">
+                <img class ="fleur" src= "public/Image/Emma/fleur_2.png"> 
+                <h1>LEMOINE EMMA</h1>
+                <img class ="fleur" src="public/Image/Emma/fleur_1.png">
+            </div>
+
+            <span>BIENVENUE SUR MA PAGE PERSONNELLE</span>
+
+
+            <!--Les boites-->
+
+            <div id = "boites">
+
+                <img class = "chat" src = "public/Image/Emma/chat_roux.png">
+
+                <div class = "apropos">
+                    <p>Bonjour ! Je m'appelle Emma et je suis actuellement étudiante en BUT métiers du multimédia et de l'internet. Créative, j'aime apporter ma touche personnelle dans les projets que j'entreprends. A l'avenir, je souhaiterais travailler dans les jeux vidéos, ma passion, mais un autre domaine qui m'intéresse particulièrement est le cinéma. Je verrais où le vent me mènera en fonction des compétences que je vais acquérir pendant mes 3 ans !</p>
+                </div>
+        
+                <div class = "moi">
+                    <img class = "illumoi" src ="public/Image/Emma/1469769_agFwzpTy.png">
+                </div>
+            </div>
+
+            
+            <!--Jeux vidéos--> 
+
+            <div class = "titre">
+                <img class = "etoile1" src ="public/Image/Emma/mario_etoile.webp">
+                <h2>MES JEUX VIDEOS PREFERES</h2>
+                <img class = "etoile2" src ="public/Image/Emma/mario_etoile.webp">
+            </div>
+
+            <div class="games">
+                <div id="zelda">
+                    <a href ="https://zelda.nintendo.com"><img src="public/Image/Emma/zelda_totk.jpg" class="vignette"></a>
+                    <p>Zelda Tears of the Kingdom</p>
+                </div>
+                <div id="splatoon">
+                    <a href ="https://splatoon.nintendo.com/fr/"><img src="public/Image/Emmasplatoon.jpg" class="vignette"></a>
+                    <p>Splatoon 3</p>
+                </div>
+                <div id="acnh">
+                    <a href ="https://animalcrossing.nintendo.com/new-horizons/fr/"><img src="public/Image/Emma/acnh.jpg" class="vignette"></a>
+                    <p>Animal Crossing New Horizon</p>
+                </div>
+            </div>
+
+
+            <!--Parcours scolaire-->
+
+                <div class ="titre">
+                    <img class ="etoile3" src ="public/Image/Emma/étoiles.png">
+                    <h2>MON PARCOURS SCOLAIRE</h2>
+                    <img class ="etoile4" src ="public/Image/Emma/étoiles.png">
+                </div>
+
+                <div class ="liste">
+                    <ul>
+                        <li>Seconde Générale</li>
+                        <img class = "fleche" src ="public/Image/Emma/flèche bas.png">
+                        <li>Choix des spécialités en Première : Maths, Physique et NSI</li>
+                        <img class = "fleche" src ="public/Image/Emma/flèche bas.png">
+                        <li>Terminale : Abandon de la spécialité maths</li>
+                        <img class = "fleche" src ="public/Image/Emma/flèche bas.png">
+                        <li>BUT MMI à l'UPEC de Sénart-Fontainebleau</li>
+                    </ul>
+                </div>
