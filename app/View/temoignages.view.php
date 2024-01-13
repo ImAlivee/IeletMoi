4c54e5834240576b43d3441ff776a4f26a46641b
Kyokanmi <superemmabros@gmail.com>
2024-01-13 16:16:07 +0100

    page témoignage

--- app/View/temoignages.view.php
+++ app/View/temoignages.view.php
@@ -1,18 +1,22 @@
 <section id = "ecrit">
     <h2>Témoignages écrits</h2>
 
-    <div class = "temoignage_ecrit">
-        <img class ="icone" src =""> 
-        <h3>Titre du témoignage</h3>
-        <p>le résumé du témoignage</p>
-        <span>Nom, prénom, age (ou anonyme)</span>
+    <div class = "temoignage">
+        <img class ="icone" src ="public\Image\Temoignage\Icone_livre.svg"> 
+        <div class = "description">
+            <h3>Titre du témoignage</h3>
+            <p>le résumé du témoignage</p>
+            <span>Nom, prénom, age (ou anonyme)</span>
+        </div>
     </div>
 
-    <div class = "temoignage_ecrit">
-        <img class ="icone" src =""> 
-        <h3>Titre du témoignage</h3>
-        <p>le résumé du témoignage</p>
-        <span>Nom, prénom, age (ou anonyme)</span>
+    <div class = "temoignage">
+        <img class ="icone" src ="public\Image\Temoignage\Icone_livre.svg"> 
+        <div class = "description">
+            <h3>Titre du témoignage</h3>
+            <p>le résumé du témoignage</p>
+            <span>Nom, prénom, age (ou anonyme)</span>
+        </div>
     </div>
 
 </section>
@@ -20,17 +24,21 @@
 <section id = "audio">
     <h2>Témoignages audio</h2>
 
-    <div class = "temoignage_audio">
+    <div class = "temoignage">
         <img class ="icone" src ="public/Image/Temoignage/Icone_audio.svg"> 
         <!--la bande son-->
-        <p>teaser du témoignage</p>
-        <span>Nom, prénom, age (ou anonyme)</span>
+        <div class = "description">
+            <p>teaser du témoignage</p>
+            <span>Nom, prénom, age (ou anonyme)</span>
+        </div>
     </div>
 
-    <div class = "temoignage_audio">
-        <img class ="icone" src ="public\Image\Temoignage\Icone_livre.svg"> 
+    <div class = "temoignage">
+        <img class ="icone" src ="public/Image/Temoignage/Icone_audio.svg"> 
         <!--la bande son-->
-        <p>teaser du témoignage</p>
-        <span>Nom, prénom, age (ou anonyme)</span>
+        <div class = "description">
+            <p>teaser du témoignage</p>
+            <span>Nom, prénom, age (ou anonyme)</span>
+        </div>
     </div>
 </section>
