8a39ab094868d8334a0b9408a32de7cd953d4f40
Tiny-Emii <emilie.becker@etu.u-pec.fr>
2024-01-19 17:31:03 +0100

    Ajout Image caroussel

--- app/View/temoignages.view.php
+++ app/View/temoignages.view.php
@@ -10,22 +10,22 @@
     <img src="public\Image\BD\2.png">
     </div>
     <div id="slide-3">
-    <img src="public\Image\Garde_BD.png">
+    <img src="public\Image\BD\3.png">
     </div>
     <div id="slide-4">
-    <img src="public\Image\Garde_BD.png">
+    <img src="public\Image\BD\4.png">
     </div>
     <div id="slide-5">
-    <img src="public\Image\Garde_BD.png">
+    <img src="public\Image\BD\5.png">
     </div>
     <div id="slide-6">
-    <img src="public\Image\Garde_BD.png">
+    <img src="public\Image\BD\6.png">
     </div>
     <div id="slide-7">
-    <img src="public\Image\Garde_BD.png">
+    <img src="public\Image\BD\7.png">
     </div>
     <div id="slide-8">
-    <img src="public\Image\Garde_BD.png">
+    <img src="public\Image\BD\8.png">
     </div>
     </div>
 </div>
