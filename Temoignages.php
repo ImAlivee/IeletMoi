f1e81bd5a87e84be3d4bb4930b592b5039c20b3d
Kyokanmi <superemmabros@gmail.com>
2024-01-20 16:41:16 +0100

    correctif test

--- Temoignages.php
+++ Temoignages.php
@@ -2,7 +2,7 @@
 $css = 'temoignage.css';
 
 ob_start();
-include 'app\View\temoignages.view.php';
+include 'app/View/temoignages.view.php';
 $content = ob_get_clean();
 
 include 'app/view/common/layout.php';
