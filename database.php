5905fa2ad56db6c99da25a88809c368adeb1cd22
Kyokanmi <superemmabros@gmail.com>
2024-01-20 18:09:26 +0100

    Update database.php

--- database.php
+++ database.php
@@ -18,7 +18,7 @@ $temoignages_bd = [
 $temoignages_oral = [
 
     1 => [
-        'audio' => '../public/Image/Temoignage/Temoignage_Emma_LEMOINE.mp3',
+        'audio' => 'public/Image/Temoignage/Temoignage_Emma_LEMOINE.mp3',
         'resume' => 'relation Parent et enfants, ce quon dis sans pensé aux autres affecte nimporte qui autour de nous qui entendrait nos propos',
         'auteur' => [
             'nom' => 'Lemoine',
@@ -28,7 +28,7 @@ $temoignages_oral = [
     ],
 
     2 => [
-        'audio' => '../public/Image/Temoignage/Temoignage_Aurore_Regnier.mp3',
+        'audio' => 'public/Image/Temoignage/Temoignage_Aurore_Regnier.mp3',
         'resume' => 'Marraine qui est passé de dire ma chérie à dire mon chérie contrairement aux parents, même si n’est pas les parents cela lui a quand même fait beaucoup de bien. Personnes qui demandent « T’es un homme ? » puis disent « Bah non ça se voit que t’en ai pas un » juste pour être méchant. Se faire interpeller par un couple dans la rue qui lui demande en boucle s’il est sûr d’être un homme.',
         'auteur' => [
             'nom' => 'Regnier',
@@ -38,7 +38,7 @@ $temoignages_oral = [
     ],
 
     3 => [
-        'audio' => '../public/Image/Temoignage/Interview_Alban_FaitoutFourmigue_GroupeM.mp3',
+        'audio' => 'public/Image/Temoignage/Interview_Alban_FaitoutFourmigue_GroupeM.mp3',
         'resume' => 'Les gens ont tendances à résumé sa bisexualité à juste être Lesbienne alors que non elle aime aussi les hommes.',
         'auteur' => [
             'nom' => 'Faitout',
@@ -47,7 +47,7 @@ $temoignages_oral = [
         ]
     ],
     4 => [
-        'audio' => '../public/Image/Temoignage/Temoignage_Emilie_BECKER_groupeM.mp3',
+        'audio' => 'public/Image/Temoignage/Temoignage_Emilie_BECKER_groupeM.mp3',
         'resume' => 'Discussion en groupe qui amène à la différence de vision entre les couples gay et lesbiens, un gars du groupe trouve les couples gay dégueulasse mais va trouver un couple lesbien chaud, la même remarque avait été faite par son père',
         'auteur' => [
             'nom' => 'Becker',
