24577f670392d9806b2b7c6f3a05ed3876fa7816
Tiny-Emii <emilie.becker@etu.u-pec.fr>
2024-01-21 11:59:25 +0100

    Modification page témoignages

--- app/View/temoignages.view.php
+++ app/View/temoignages.view.php
@@ -1,37 +1,3 @@
-<section  id ="BD">
-<h3>Titre de la BD</h3>
-
-<div>
-    <div class="slider">
-    <div id="slide-1">
-      <img src="public\Image\BD\1.png">
-    </div>
-    <div id="slide-2">
-    <img src="public\Image\BD\2.png">
-    </div>
-    <div id="slide-3">
-    <img src="public\Image\BD\3.png">
-    </div>
-    <div id="slide-4">
-    <img src="public\Image\BD\4.png">
-    </div>
-    <div id="slide-5">
-    <img src="public\Image\BD\5.png">
-    </div>
-    <div id="slide-6">
-    <img src="public\Image\BD\6.png">
-    </div>
-    <div id="slide-7">
-    <img src="public\Image\BD\7.png">
-    </div>
-    <div id="slide-8">
-    <img src="public\Image\BD\8.png">
-    </div>
-    </div>
-</div>
-
-</section>
-
 <section id = "ecrit">
     <h2>Témoignages écrits</h2>
 
@@ -78,4 +44,39 @@
             </div>
         </div>
     <?php endforeach; ?>
+</section>
+<section  id ="BD">
+    <h2>Témoignages BD</h2>
+
+    <h3>Titre de la BD</h3>
+
+    <div>
+        <div class="slider">
+        <div id="slide-1">
+        <img src="public\Image\BD\1.png">
+        </div>
+        <div id="slide-2">
+        <img src="public\Image\BD\2.png">
+        </div>
+        <div id="slide-3">
+        <img src="public\Image\BD\3.png">
+        </div>
+        <div id="slide-4">
+        <img src="public\Image\BD\4.png">
+        </div>
+        <div id="slide-5">
+        <img src="public\Image\BD\5.png">
+        </div>
+        <div id="slide-6">
+        <img src="public\Image\BD\6.png">
+        </div>
+        <div id="slide-7">
+        <img src="public\Image\BD\7.png">
+        </div>
+        <div id="slide-8">
+        <img src="public\Image\BD\8.png">
+        </div>
+        </div>
+    </div>
+
 </section>
