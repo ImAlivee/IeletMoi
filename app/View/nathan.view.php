ea162554c6108f984677124f3f767fe3b4893de5
RedEmperor7 <73673106+RedEmperor7@users.noreply.github.com>
2024-01-22 15:48:03 +0100

    presentation_touches

--- app/View/nathan.view.php
+++ app/View/nathan.view.php
@@ -4,7 +4,7 @@
     I love videogames, movies, books and enjoy making them just as much i do playing/watching/reading them
 
 </p>
-<section>
+<section class="projects">
     <h3>Some of my projects</h3>
     <ul>
         <li class="image"></li>
@@ -12,28 +12,28 @@
     </ul>
 </section>
 <ul class="favmedia">
-    <li>
+    <li class="cards">
         <section class="games">
-            <h3>Fav Games</h3>
-            <ul>
+            <h3>Favorite Games</h3>
+            <ul class="liste">
                 <li class="image"><img src="https://imagedelivery.net/JAV112JY973Crznn4xb8Sg/cbd1d063-fe13-4eb6-8e38-901b34b02000/public" alt=""></li>
                 <li class="image"></li>
             </ul>
         </section>
     </li>
-    <li>
+    <li class="cards">
         <section class="movies">
-            <h3>Fav Movies</h3>
-            <ul>
+            <h3>Favorite Movies</h3>
+            <ul class="liste">
                 <li class="image"></li>
                 <li class="image"></li>
             </ul>
         </section>
     </li>
-    <li>
+    <li class="cards">
         <section class="books">
-            <h3>Fav Books</h3>
-            <ul>
+            <h3>Favorite Books</h3>
+            <ul class="liste">
                 <li class="image"><img src="https://m.media-amazon.com/images/I/81T7x8izVQL._AC_UF1000,1000_QL80_.jpg" alt=""></li>
                 <li class="image"></li>
             </ul>
