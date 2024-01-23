604a31c2773efe9914fad18f1d0edd0650e0f534
RedEmperor7 <73673106+RedEmperor7@users.noreply.github.com>
2024-01-23 23:30:43 +0100

    perso_changes

--- app/View/nathan.view.php
+++ app/View/nathan.view.php
@@ -1,6 +1,6 @@
 <h2>Nathân Moudio</h2>
 
-<section class="photo"><img src="https://media.discordapp.net/attachments/653200595242713120/1199030867125878985/EMP_AVATAR.png?ex=65c10f83&is=65ae9a83&hm=b9722f6b31e659544a4537a21c390a87cc5ae2594df7a0f3edf020f308da3c73&=&format=webp&quality=lossless&width=565&height=565" alt=""></section>
+<section class="photo"><img src="public\image\nathan\empavatar.webp" alt=""></section>
 <p>
     I'm a BUT1 MMI student,
     I love videogames, movies, books and enjoy making them just as much i do playing/watching/reading them
@@ -13,10 +13,10 @@
 <section class="projects">
     <h3>Some projects of mine</h3>
     <ul class="liste">
-        <li class="image"><img src="https://media.discordapp.net/attachments/924791641766789161/1153427423992500274/image.png?ex=65b80a7f&is=65a5957f&hm=d83e822e075e11e5c798046ca04d4b2e2d28413b24611e3fc12ff4d8bed9bcb9&=&format=webp&quality=lossless&width=823&height=565" alt=""></li>
-        <li class="image"><img src="https://media.discordapp.net/attachments/924791641766789161/998430433115111474/unknown.png?ex=65c0554d&is=65ade04d&hm=098f5492bf0eb3359ba7c85f7a14bb506ceb01e781797e18b5475eea94fe0032&=&format=webp&quality=lossless&width=573&height=565" alt=""></li>
-        <li class="image"><img src="https://media.discordapp.net/attachments/924791641766789161/998431398786502746/unknown.png?ex=65c05633&is=65ade133&hm=a9eb188d457124a1524af2fa58c0cd03a07edac5a9ea3eef0fab9b401ffe38b4&=&format=webp&quality=lossless&width=748&height=565" alt=""></li>
-        <li class="image"><img src="https://media.discordapp.net/attachments/924791641766789161/957703209026215976/unknown.png?ex=65bfd31e&is=65ad5e1e&hm=f89648a60648b5b944c78d3c245b878a6fd9d7190d9555dc0eb57c2d4e1c9295&=&format=webp&quality=lossless&width=963&height=565" alt=""></li>
+        <li class="image"><img src="public\image\nathan\converse_boy.webp" alt=""></li>
+        <li class="image"><img src="public\image\nathan\dog_wink.webp" alt=""></li>
+        <li class="image"><img src="public\image\nathan\dog.webp" alt=""></li>
+        <li class="image"><img src="public\image\nathan\spiderman_render.webp" alt=""></li>
         <li class="image"><img src="" alt=""></li>
     </ul>
 </section>
@@ -25,9 +25,9 @@
         <section class="games">
             <h3>Favorite Games</h3>
             <ul class="liste">
-                <li class="image"><img src="https://imagedelivery.net/JAV112JY973Crznn4xb8Sg/cbd1d063-fe13-4eb6-8e38-901b34b02000/public" alt=""></li>
-                <li class="image"><img src="https://gaming-cdn.com/images/products/1273/orig/titanfall-2-pc-jeu-ea-app-cover.jpg?v=1703158431" alt=""></li>
-                <li class="image"><img src="https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_SuperSmashBrosUltimate_02_image1600w.jpg" alt=""></li>
+                <li class="image"><img src="public\image\nathan\splatoon3.avif" alt=""></li>
+                <li class="image"><img src="public\image\nathan\titanfall2.jpg" alt=""></li>
+                <li class="image"><img src="public\image\nathan\ssbu.jpg" alt=""></li>
                 <li class="image"><img src="https://cdn-products.eneba.com/resized-products/_xhcfDFbvlJ1OQKnNrAZBhFYKMKOj5C4Mjqrgk9M27I_1920x1080_1x-0.jpg" alt=""></li>
                 <li class="image"><img src="https://upload.wikimedia.org/wikipedia/en/9/99/Sonic_Adventure_2_cover.png" alt=""></li>
                 <li class="image"><img src="https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_ds_22/SI_NDS_InazumaEleven_image1600w.jpg" alt=""></li>
@@ -40,7 +40,7 @@
         <section class="movies">
             <h3>Favorite Movies and Shows</h3>
             <ul class="liste">
-                <li class="image"><img src="https://fr.web.img2.acsta.net/pictures/23/05/11/10/00/1986933.jpg" alt=""></li>
+                <li class="image"><img src="public\image\nathan\spiderman_atsv.webp" alt=""></li>
                 <li class="image"><img src="https://lepauledorion.files.wordpress.com/2022/01/arcane.jpeg" alt=""></li>
                 <li class="image"><img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/88F69C68DD6CF663D864879073BFD6BBDC3D2F7707FEFC7D2D1096D79911DC07/scale?width=1200&aspectRatio=1.78&format=jpeg" alt=""></li>
                 <li class="image"><img src="https://images-na.ssl-images-amazon.com/images/S/pv-target-images/9eb0363e04337d8db95651f9244f501769853ffb107a541d30cf637d651472d6._RI_TTW_.jpg" alt=""></li>
